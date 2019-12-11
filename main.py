951fb2fefb54b65cc084819794d7eacc6c713f20
Mookyung Kwak <pporri@yonsei.ac.kr>
2019-12-11 20:45:44 +0900

    fixing

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from ortools.sat.python import cp_model
 class MembersPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
     """Print intermediate solutions."""
 
-    def __init__(self, shifts, num_members, num_days, num_shifts, sols):
+    def __init__(self, shiftsA, shiftsB, num_members, num_days, num_shifts, sols):
         cp_model.CpSolverSolutionCallback.__init__(self)
-        self._shifts = shifts
+        self._shiftsA = shiftsA
+        self._shiftsB = shiftsB
         self._num_members = num_members
         self._num_days = num_days
         self._num_shifts = num_shifts
@@ -21,9 +22,12 @@ class MembersPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
                 for n in range(self._num_members):
                     is_working = False
                     for s in range(self._num_shifts):
-                        if self.Value(self._shifts[(n, d, s)]):
+                        if self.Value(self._shiftsA[(n, d, s)]):
                             is_working = True
-                            print('  Nurse %i works shift %i' % (n, s))
+                            print('  Member %i works shiftA %i' % (n, s))
+                        if self.Value(self._shiftsB[(n, d, s)]):
+                            is_working = True
+                            print('  Member %i works shiftB %i' % (n, s))
                     if not is_working:
                         print('  Member {} does not work'.format(n))
             print()
@@ -34,50 +38,65 @@ class MembersPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
 
 def main():
     names = ['박호원', '이보람', '정형섭', '남영선', '양혜경', '이찬희', '이장훈', '조인경']
+    # names = ['박호원', '이보람', '정형섭']
     num_days = 31
-    num_shifts = 2
+    num_shiftsA = 1
+    num_shiftsB = 1
     num_members = len(names)
 
     all_members = range(num_members)
+    all_shiftsA = range(num_shiftsA)
+    all_shiftsB = range(num_shiftsB)
+    num_shifts = num_shiftsA + num_shiftsB
     all_shifts = range(num_shifts)
     all_days = range(num_days)
 
-    CONST_X = 10
-    MIN_A = 2
-    MAX_A = 2
-    stack_x = [10, 10, 10, 10, 10, 10, 10, 10]
-
     model = cp_model.CpModel()
-    shifts = {}
+    shiftsA = {}
+    shiftsB = {}
     for n in all_members:
         for d in all_days:
-            for s in all_shifts:
-                shifts[(n, d, s)] = model.NewBoolVar('shift_n%id%is%i' % (n, d, s))
+            shiftsA[(n, d, 0)] = model.NewBoolVar('shiftA_n%id%is%i' % (n, d, 0))
+            shiftsB[(n, d, 0)] = model.NewBoolVar('shiftB_n%id%is%i' % (n, d, 0))
+            print('shiftA_n%id%is%i' % (n, d, 0))
+            print(shiftsA[(n, d, 0)].Name())
+            print('shiftB_n%id%is%i' % (n, d, 0))
+            print(shiftsB[(n, d, 0)].DebugString())
 
     for d in all_days:
-        for s in all_shifts:
-            model.Add(sum(shifts[(n, d, s)] for n in all_members) == 2)
+        for a in all_shiftsA:
+            model.Add(sum(shiftsA[(n, d, a)] for n in all_members) == 1)
+        for b in all_shiftsB:
+            model.Add(sum(shiftsB[(n, d, b)] for n in all_members) == 1)
 
     for n in all_members:
         for d in all_days:
-            model.Add(sum(shifts[(n, d, s)] for s in all_shifts) <= 1)
+            model.Add(sum(shiftsA[(n, d, a)] for a in all_shiftsA) <= 1)
+            model.Add(sum(shiftsB[(n, d, b)] for b in all_shiftsB) <= 1)
+            model.Add( not (shiftsA[(n, d, 0)] == 1 and shiftsB[(n, d, 0)] == 1))
 
     # min_shifts_per_member is the largest integer such that every nurse
     # can be assigned at least that many shifts. If the number of nurses doesn't
     # divide the total number of shifts over the schedule period,
     # some nurses have to work one more shift, for a total of
     # min_shifts_per_member + 1.
-    min_shifts_per_member = (num_shifts * num_days) // num_members
-    max_shifts_per_member = min_shifts_per_member + 3
+    min_shiftsA_per_member = (num_shiftsA * num_days) // num_members
+    min_shiftsB_per_member = (num_shiftsB * num_days) // num_members
+    print("min_shifts_per_member", min_shiftsA_per_member)
+    max_shiftsA_per_member = min_shiftsA_per_member + 1
+    max_shiftsB_per_member = min_shiftsB_per_member + 1
     for n in all_members:
-        num_shifts_worked = sum(shifts[(n, d, s)] for d in all_days for s in all_shifts)
-        model.Add(min_shifts_per_member <= num_shifts_worked)
-        model.Add(num_shifts_worked <= max_shifts_per_member)
+        num_shiftsA_worked = sum(shiftsA[(n, d, 0)] for d in all_days)
+        num_shiftsB_worked = sum(shiftsB[(n, d, 0)] for d in all_days)
+        model.Add(min_shiftsA_per_member <= num_shiftsA_worked)
+        model.Add(min_shiftsB_per_member <= num_shiftsB_worked)
+        model.Add(num_shiftsA_worked <= max_shiftsA_per_member)
+        model.Add(num_shiftsB_worked <= max_shiftsB_per_member)
 
     solver = cp_model.CpSolver()
     solver.parameters.linearization_level = 0
     a_few_solutions = range(5)
-    solution_printer = MembersPartialSolutionPrinter(shifts, num_members, num_days, num_shifts, a_few_solutions)
+    solution_printer = MembersPartialSolutionPrinter(shiftsA, shiftsB, num_members, num_days, num_shifts, a_few_solutions)
     solver.SearchForAllSolutions(model, solution_printer)
 
     print()
@@ -85,7 +104,7 @@ def main():
     print(' - conflicts             : %i' % solver.NumConflicts())
     print(' - branches              : %i' % solver.NumBranches())
     print(' - wall time             : %f s' % solver.WallTime())
-    print(' - solutions ffound      : %i' % solution_printer.solution_count())
+    print(' - solutions found       : %i' % solution_printer.solution_count())
 
 if __name__ == '__main__':
     main()
